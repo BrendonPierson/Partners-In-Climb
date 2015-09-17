48263d46c9110696fec823f2a884e218248b8991
BrendonPierson <brendonpierson@gmail.com>
2015-09-17 10:26:45 -0500

    climb edit details auto update on panel close

--- src/app/singleCragView/singleCrag.js
+++ src/app/singleCragView/singleCrag.js
@@ -28,6 +28,9 @@
       // download the data into local objects
       var crags = $firebaseArray(ref.child('areas'));
       var climbs = $firebaseArray(ref.child('climbs'));
+      $scope.bolt_types = $firebaseArray(ref.child('hardware'));
+
+
       
       // Promises for fb data
       crags.$loaded()
@@ -55,34 +58,36 @@
           };
         })
 
-    // add a new area
+    // add a new route
     $scope.newRoute = {};
 
     $scope.addRoute = function(){
       $scope.newRoute.createdBy = currentAuth.uid;
       $scope.newRoute.crag_id = $stateParams.id;
       $scope.newRoute.dateAdded = new Date();
+
       climbs.$add($scope.newRoute).then(function(ref) {
         var id = ref.key();
         console.log("added record with id " + id);
         $scope.newRoute = {};
         foundationApi.publish('addRouteModal', 'close');
       });
+
     }
 
     $scope.setPanelDetails = function(climb){
-      $scope.climbName = climb.name;
-      $scope.image_url = climb.image_url;
-      $scope.sector = climb.sector;
-      $scope.bolted_by = climb.bolted_by;
-      $scope.bolt_condition = climb.bolt_condition;
-      $scope.last_bolted = climb.last_bolted;
-      $scope.grade = climb.grade;
-      $scope.bolt_type = climb.bolt_type;
-      $scope.description = climb.description;
-      $scope.fixed_gear = climb.fixed_gear;
+      $scope.climb = climb;
     }
 
+    $scope.submitClimbEdits = function(){
+      console.log("climb", $scope.climb);
+      climbs[$scope.climb.$id] = $scope.climb;
+      console.log("climb", climbs[$scope.climb.$id]);
+
+      climbs.$save($scope.climb).then(function(ref) {
+        console.log("ref", ref);
+      });
+    }
 
 
 
